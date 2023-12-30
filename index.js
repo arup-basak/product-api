8740664ae0a84e154154d400820f2ea4b8f504b2
Arup Basak <email.arupbasak@gmail.com>
2023-12-30 20:37:12 +0530

    added prisma

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
-import express from 'express'
-import 'dotenv/config'
+import express from "express";
+import "dotenv/config";
+import { PrismaClient } from "@prisma/client";
 
 const app = express();
+const prisma = new PrismaClient();
 
-const PORT = process.env.PORT || 4000
+const PORT = process.env.PORT || 4000;
 
 app.get("/", (req, res) => {
-    res.json({msg: "Hello World"})
-})
+  res.json({ msg: "Hello World" });
+});
 
 app.listen(PORT, () => {
-    console.log(` > LISTEN TO PORT ${PORT}`)
-})
+  console.log(` > LISTEN TO PORT ${PORT}`);
+});
